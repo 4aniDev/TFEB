57b727f5c3cc8c8533c1fc9e13baae22b20081f7
Dan <tm.test@mail.ru>
2022-05-02 14:04:45 +0500

    Create SettingsFragment.kt

---
+++ app/src/main/java/ru/chani/tfeb/presentation/SettingsFragment.kt
@@ -0,0 +1,31 @@
+package ru.chani.tfeb.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.chani.tfeb.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding: FragmentSettingsBinding
+        get() = _binding ?: throw RuntimeException("FragmentSettingsBinding = null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    companion object {
+
+        fun newInstanceSettingsFragment() = SettingsFragment()
+    }
+
+}
